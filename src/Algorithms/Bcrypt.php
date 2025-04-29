56081ac6b8f0599524240f5db53cf1fd3afe1366
gagiyin <76633954+gagiyin@users.noreply.github.com>
2025-04-29 15:28:25 +0200

    Made some updateal fixes for Bcrypt.php
    Some variables stuck in self::variable except of $this->variable in classes.

--- src/Algorithms/Bcrypt.php
+++ src/Algorithms/Bcrypt.php
@@ -1,4 +1,39 @@
 <?php
+
+/*
+Its me, gagiyin. I made some fixes on the code, because I had a few errors located in Bcrypt class. 
+
+What i fixed:
+
+--
+
+Error1:
+	$cost = self::$_defaultCost; <- i had errors with this
+Fix1: 	
+	if (empty($cost)) {
+	    $cost = $this->_defaultCost;
+	}
+
+--
+
+Error2:
+	return substr($salt, 0, self::$_saltLength);
+Fix2:
+	return substr($salt, 0, $this->_saltLength);
+--
+
+Error3:
+	return sprintf('$%s$%02d$%s$', self::$_saltPrefix, $cost, $salt);
+Fix3:
+	return sprintf('$%s$%02d$%s$', $this->_saltPrefix, $cost, $salt);
+
+--
+
+Reason: $_defaultCost is not static, but an objective variable, so it needs $this->_defaultCost instead of self::(..)
+Hope it works, respect to the authors
+
+*/
+
 /*
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -23,91 +58,89 @@
  */
 class Bcrypt extends Algorithm {
 
-	/**
-	 * Default salt prefix
-	 * 
-	 * @see http://www.php.net/security/crypt_blowfish.php
-	 * 
-	 * @var string
-	 */
-	protected $_saltPrefix = '2a';
-	 
-	/**
-	 * Default hashing cost (4-31)
-	 * 
-	 * @var integer
-	 */
-	protected $_defaultCost = 14;
- 
-	/**
-	 * Salt limit length
-	 * 
-	 * @var integer
-	 */
-	protected $_saltLength = 22;
+    /**
+     * Default salt prefix
+     * 
+     * @see http://www.php.net/security/crypt_blowfish.php
+     * 
+     * @var string
+     */
+    protected $_saltPrefix = '2a';
+    
+    /**
+     * Default hashing cost (4-31)
+     * 
+     * @var integer
+     */
+    protected $_defaultCost = 14;
 
-	/**
-	 * Hash a string
-	 * 
-	 * @param  string  $string The string
-	 * @param  integer $cost   The hashing cost
-	 * 
-	 * @see	http://www.php.net/manual/en/function.crypt.php
-	 * 
-	 * @return string
-	 */
-	public function hash(string $string, $cost = null) {
-		if (empty($cost)) {
-			$cost = self::$_defaultCost;
-		}
- 
-		// Salt
-		$salt = $this->generate_random_salt();
- 
-		// Hash string
-		$hashString = $this->__generate_hash_string((int)$cost, $salt);
- 
-		return crypt($string, $hashString);
-	}
+    /**
+     * Salt limit length
+     * 
+     * @var integer
+     */
+    protected $_saltLength = 22;
 
-	/**
-	 * Check a hashed string
-	 * 
-	 * @param  string $string The string
-	 * @param  string $hash   The hash
-	 * 
-	 * @return boolean
-	 */
-	public function verify(string $password, string $hash) {
-		return (crypt($password, $hash) === $hash);
-	}
+    /**
+     * Hash a string
+     * 
+     * @param  string  $string The string
+     * @param  integer $cost   The hashing cost
+     * 
+     * @return string
+     */
+    public function hash(string $string, $cost = null) {
+        if (empty($cost)) {
+            $cost = $this->_defaultCost;
+        }
 
-	/**
-	 * Generate a random base64 encoded salt
-	 * 
-	 * @return string
-	 */
-	private function generate_random_salt() {
-		// Salt seed
-		$seed = uniqid(mt_rand(), true);
- 
-		// Generate salt
-		$salt = base64_encode($seed);
-		$salt = str_replace('+', '.', $salt);
- 
-		return substr($salt, 0, self::$_saltLength);
-	}
- 
-	/**
-	 * Build a hash string for crypt()
-	 * 
-	 * @param  integer $cost The hashing cost
-	 * @param  string $salt  The salt
-	 * 
-	 * @return string
-	 */
-	private function __generate_hash_string($cost, $salt) {
-		return sprintf('$%s$%02d$%s$', self::$_saltPrefix, $cost, $salt);
-	}
+        // Salt
+        $salt = $this->generate_random_salt();
+
+        // Hash string
+        $hashString = $this->__generate_hash_string((int)$cost, $salt);
+
+        return crypt($string, $hashString);
+    }
+
+    /**
+     * Check a hashed string
+     * 
+     * @param  string $string The string
+     * @param  string $hash   The hash
+     * 
+     * @return boolean
+     */
+    public function verify(string $password, string $hash) {
+        return (crypt($password, $hash) === $hash);
+    }
+
+    /**
+     * Generate a random base64 encoded salt
+     * 
+     * @return string
+     */
+    private function generate_random_salt() {
+        // Salt seed
+        $seed = uniqid(mt_rand(), true);
+
+        // Generate salt
+        $salt = base64_encode($seed);
+        $salt = str_replace('+', '.', $salt);
+
+        return substr($salt, 0, $this->_saltLength);
+    }
+
+    /**
+     * Build a hash string for crypt()
+     * 
+     * @param  integer $cost The hashing cost
+     * @param  string $salt  The salt
+     * 
+     * @return string
+     */
+    private function __generate_hash_string($cost, $salt) {
+        return sprintf('$%s$%02d$%s$', $this->_saltPrefix, $cost, $salt);
+    }
 
 }
